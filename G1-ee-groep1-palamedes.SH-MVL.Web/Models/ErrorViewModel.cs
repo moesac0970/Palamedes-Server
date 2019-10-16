30131c62c6fc92823c58e9dde18300313d388e7c
BaronSepp <SeppeHollants@pm.me>
2019-10-16 12:14:21 +0200

    added identity scaffolded mvc project

---
+++ G1-ee-groep1-palamedes.SH-MVL.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace G1_ee_groep1_palamedes.SH_MVL.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
