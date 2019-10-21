14e0a7b31afd0ee820a89418710e262fe9b308e8
moesac0970 <manu09ice@gmail.com>
2019-10-21 20:43:08 +0200

    startup

--- G1-ee-groep1-palamedes.SH-MVL.API/Startup.cs
+++ G1-ee-groep1-palamedes.SH-MVL.API/Startup.cs
@@ -62,11 +62,9 @@ namespace G1_ee_groep1_palamedes.SH_MVL.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
 
-            }
+            
 
             app.UseRouting();
 
