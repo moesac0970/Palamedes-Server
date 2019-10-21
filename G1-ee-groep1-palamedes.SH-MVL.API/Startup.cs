95d7e2a578da1978fd3aeae50014d6e337fd38e2
moesac0970 <manu09ice@gmail.com>
2019-10-21 19:57:54 +0200

    merge fault startup

--- G1-ee-groep1-palamedes.SH-MVL.API/Startup.cs
+++ G1-ee-groep1-palamedes.SH-MVL.API/Startup.cs
@@ -62,8 +62,8 @@ namespace G1_ee_groep1_palamedes.SH_MVL.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
 
             }
