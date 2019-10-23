4638155bfcd474ca5ace73525798f350f09bf713
moesac0970 <manu09ice@gmail.com>
2019-10-23 17:30:07 +0200

    identy scaffold

---
+++ G1-ee-groep1-palamedes.SH-MVL.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using G1_ee_groep1_palamedes.SH_MVL.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(G1_ee_groep1_palamedes.SH_MVL.Web.Areas.Identity.IdentityHostingStartup))]
+namespace G1_ee_groep1_palamedes.SH_MVL.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
