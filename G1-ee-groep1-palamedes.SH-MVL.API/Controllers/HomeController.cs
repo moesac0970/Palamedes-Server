bbbfdcffdda8066970747ce754915e12c66e70cb
BaronSepp <SeppeHollants@pm.me>
2019-11-05 14:18:24 +0100

    code cleanup

--- G1-ee-groep1-palamedes.SH-MVL.API/Controllers/HomeController.cs
+++ G1-ee-groep1-palamedes.SH-MVL.API/Controllers/HomeController.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace G1_ee_groep1_palamedes.SH_MVL.API.Controllers
 {
     [Route("/")]
     public class HomeController : Controller
-    {      
+    {
         // Enables polling to api base domain 
-        [HttpGet]
         public IActionResult Get()
         {
             return StatusCode(200); // Returns an empty page
