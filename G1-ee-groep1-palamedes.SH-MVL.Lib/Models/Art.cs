7c7c538effeb142a8b7c48f23b8e31e515410e2e
BaronSepp <SeppeHollants@pm.me>
2019-11-11 03:13:39 +0100

    Added DataAnnotations + RegexExtensions

--- G1-ee-groep1-palamedes.SH-MVL.Lib/Models/Art.cs
+++ G1-ee-groep1-palamedes.SH-MVL.Lib/Models/Art.cs
@@ -1,18 +1,34 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace G1_ee_groep1_palamedes.SH_MVL.Lib.Models
 {
     public class Art : EntityBase
     {
+        
+        [DataType(DataType.Text)]
+        [StringLength(30, MinimumLength = 3)]
         public string Name { get; set; }
+        
         public int CategoryId { get; set; }
+        
         public Category Category { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Created { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+        
+        [DataType(DataType.Date)]
         public DateTime Updated { get; set; }
+        
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
+        
+        [DataType(DataType.ImageUrl)]
         public string ImageName { get; set; }
+
 
         public int ArtistId { get; set; }
         public Artists Artist { get; set; }
--- G1-ee-groep1-palamedes.SH-MVL.Lib/Models/User.cs
+++ G1-ee-groep1-palamedes.SH-MVL.Lib/Models/User.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace G1_ee_groep1_palamedes.SH_MVL.Lib.Models
 {
     public class User : EntityBase
     {
+        [DataType(DataType.Text)]
+        [StringLength(15, MinimumLength = 3)]
         public string FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(15, MinimumLength = 3)]
         public string LastName { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
         public DateTime Dob { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
         public int TelNr { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(10, MinimumLength = 4)]
         public string UserName { get; set; }
+
         public string HashPasw { get; set; }
 
     }
