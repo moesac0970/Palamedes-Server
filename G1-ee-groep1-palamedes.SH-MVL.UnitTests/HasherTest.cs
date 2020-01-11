c860915a470d974b8b3cd3889c6f0d0c7ce0c5cd
BaronSepp <SeppeHollants@pm.me>
2020-01-11 16:54:23 +0100

    Added blank NUnit project

---
+++ G1-ee-groep1-palamedes.SH-MVL.UnitTests/HasherTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace G1_ee_groep1_palamedes.SH_MVL.UnitTests
+{
+    public class HasherTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
