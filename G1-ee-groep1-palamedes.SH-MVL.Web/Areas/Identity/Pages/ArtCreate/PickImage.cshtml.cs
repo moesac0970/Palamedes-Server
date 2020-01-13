f23547f041fe9b8de487a5e7c5550faf6fa3a8d1
moesac0970 <42092816+moesac0970@users.noreply.github.com>
2020-01-13 03:10:01 +0100

    pickimage

--- G1-ee-groep1-palamedes.SH-MVL.Web/Areas/Identity/Pages/ArtCreate/PickImage.cshtml.cs
+++ G1-ee-groep1-palamedes.SH-MVL.Web/Areas/Identity/Pages/ArtCreate/PickImage.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using G1_ee_groep1_palamedes.SH_MVL.API.Models;
 using G1_ee_groep1_palamedes.SH_MVL.Web.Helper;
@@ -32,17 +33,32 @@ namespace G1_ee_groep1_palamedes.SH_MVL.Web.Areas.Identity.Pages.ArtCreate
         {
         }
 
-        [ValidateAntiForgeryToken]
         [HttpPost]
-        public async Task<IActionResult> UploadFile(IFormFile file)
+        public async Task<IActionResult> OnPostAsync(IFormFile file)
         {
             var user = await _userManager.FindByNameAsync(_signInManager.Context.User.Identity.Name);
-            Artist artist = WebApiHelper.GetApiResult<Artist>(baseUri + "artistbyuserid/" + user.Id);
-
+            Artist artist = new Artist();
+            artist = WebApiHelper.GetApiResult<Artist>(baseUri+ "users/artistbyuserid/" + user.Id);
+            
+            baseUri += "arts/ImageArtistName/" + artist.ArtistName;
             HttpClient httpClient = new HttpClient();
+            string token = HttpContext.Request.Cookies["bearerToken"];
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
 
+            ////
+            
+            byte[] data;
+            using (var br = new BinaryReader(file.OpenReadStream()))
+            {
+                data = br.ReadBytes((int)file.OpenReadStream().Length);
+            }
+            ByteArrayContent bytes = new ByteArrayContent(data);
+            MultipartFormDataContent multiContent = new MultipartFormDataContent();
+            multiContent.Add(bytes, "file", file.FileName);
+            
+            var response = await httpClient.PostAsync(baseUri, multiContent);
+            return RedirectToAction("Index", "Home");
 
-            return RedirectToAction("index", "Home");
 
         }
     }
